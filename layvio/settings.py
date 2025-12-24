42b21fadb0208791f356f78f9a3cc29ceae744d8
gajenderdeveloper <gajender.developer@gmail.com>
2025-12-24 08:43:46 +0530

    changes vercel

--- layvio/settings.py
+++ layvio/settings.py
@@ -141,7 +141,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Or any other folder name like 'staticfiles_build'
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
 
 
 
