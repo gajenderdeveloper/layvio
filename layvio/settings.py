624de4a12561510a80126beb2705a084ef37ec71
gajenderdeveloper <gajender.developer@gmail.com>
2026-03-08 09:51:50 +0530

    db_setting

--- layvio/settings.py
+++ layvio/settings.py
@@ -159,7 +159,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
